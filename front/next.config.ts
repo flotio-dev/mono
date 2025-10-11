98f497a49e5bf8492db5d30d59240566e5f054ab
garder500 <jeremy27.clara22@gmail.com>
2025-10-11 23:33:29 +0200

    chore: upgrade Next.js to 16.0.0-canary.1 and update dependencies
    - Updated Next.js version in package.json to ^16.0.0-canary.1.
    - Changed tailwindcss version to ^4 and updated postcss configuration to use @tailwindcss/postcss.
    - Modified globals.css to use @layer for theme, base, mui, components, and utilities.
    - Removed tailwind.config.js as its configuration is now handled in globals.css.
    - Updated tsconfig.json to use "react-jsx" for JSX and added additional include paths.
    - Changed SERVER_URL in organization-service/.env.example from :8081 to :8082.

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    // These are all the locales you want to support in
-    // your application
-    locales: ['en', 'fr',],
-    defaultLocale: 'fr',
-  },
   output: 'standalone',
   // Ensure Next infers the correct workspace root (avoids picking up a parent lockfile)
 };
